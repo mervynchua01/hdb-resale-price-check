9e4595b9c35cdb23c8846a0410a61fef64f17995
mervynchua01 <mervynchua01@gmail.com>
2026-03-07 01:01:31 +0800

    grid selections

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,64 @@
-// src/App.jsx
+import "./App.css";
+import { useState } from "React";
+
+const TOWNS = [
+  "ANG MO KIO",
+  "BEDOK",
+  "BISHAN",
+  "BUKIT BATOK",
+  "BUKIT MERAH",
+  "BUKIT PANJANG",
+  "BUKIT TIMAH",
+  "CENTRAL AREA",
+  "CHOA CHU KANG",
+  "CLEMENTI",
+  "GEYLANG",
+  "HOUGANG",
+  "JURONG EAST",
+  "JURONG WEST",
+  "KALLANG/WHAMPOA",
+  "MARINE PARADE",
+  "PASIR RIS",
+  "PUNGGOL",
+  "QUEENSTOWN",
+  "SEMBAWANG",
+  "SENGKANG",
+  "SERANGOON",
+  "TAMPINES",
+  "TOA PAYOH",
+  "WOODLANDS",
+  "YISHUN",
+];
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [selected, setSelected] = useState([]);
+
+  const toggleTown = (town) => {
+    if (selected.includes(town)) {
+      setSelected(selected.filter((t) => t !== town));
+    } else {
+      setSelected([...selected, town]);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Pick your neighbourhoods</h1>
+      <div className="town-grid">
+        {TOWNS.map((town) => (
+          <div
+            key={town}
+            className={
+              selected.includes(town) ? "town-card active" : "town-card"
+            }
+            onClick={() => toggleTown(town)}
+          >
+            {town}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default App;
